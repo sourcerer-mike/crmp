d0f1ef7bd72dce3c591216a8ccd1f353be8ec4a0
Mike Pretzlaw <pretzlaw@gmail.com>
2016-01-16 15:39:31 +0100

    Twig: Price extension

---
+++ src/AppBundle/Twig/AppExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class AppExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('price', array($this, 'priceFilter')),
+        );
+    }
+
+    public function getName()
+    {
+        return 'app_extension';
+    }
+
+    public function priceFilter($number, $decimals = 2, $decPoint = ',', $thousandsSep = '.')
+    {
+        $price = number_format($number, $decimals, $decPoint, $thousandsSep);
+        $price = $price . ' EUR';
+
+        return $price;
+    }
+}
