976bae0a77c53f442d8f284fae84fac31a9f394d
Mike Pretzlaw <pretzlaw@gmail.com>
2016-01-29 17:05:16 +0100

    Swap fields

--- src/Crmp/AccountingBundle/Form/InvoiceType.php
+++ src/Crmp/AccountingBundle/Form/InvoiceType.php
@@ -15,8 +15,8 @@ class InvoiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('value')
 	        ->add('customer')
+            ->add('value')
         ;
     }
     
