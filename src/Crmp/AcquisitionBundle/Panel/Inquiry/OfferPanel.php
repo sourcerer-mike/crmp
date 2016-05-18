7d9340329eb07ec626a596e937ca220830b6000e
Mike Pretzlaw <pretzlaw@gmail.com>
2016-05-18 21:59:11 +0200

    inquiry_show: Panel with a list of offers below inquiry

---
+++ src/Crmp/AcquisitionBundle/Panel/Inquiry/OfferPanel.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Crmp\AcquisitionBundle\Panel\Inquiry;
+
+
+use Crmp\AcquisitionBundle\Entity\Inquiry;
+use Crmp\CrmBundle\Twig\AbstractPanel;
+use Crmp\CrmBundle\Twig\PanelInterface;
+
+class OfferPanel extends AbstractPanel implements PanelInterface
+{
+    public function getData()
+    {
+        if ($this->data) {
+            return (array) $this->data;
+        }
+
+        $this->data           = $this->container->get('crmp.controller.render.parameters');
+        $this->data['offers'] = [];
+
+        if ( ! isset( $this->data['inquiry'] ) || false == ( $this->data['inquiry'] instanceof Inquiry )) {
+            return $this->data;
+        }
+
+        /** @var Inquiry $inquiry */
+        $inquiry    = $this->data['inquiry'];
+        $addressRepo = $this->container->get('doctrine')->getRepository('AcquisitionBundle:Offer');
+
+        $this->data['offers'] = $addressRepo->findBy(
+            [
+                'inquiry' => $inquiry,
+            ],
+            null,
+            10
+        );
+
+        return $this->data;
+    }
+
+    /**
+     * Return a unique identifier among all known boardlets.
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return 'acquisition_offer_list';
+    }
+
+    public function getTemplate()
+    {
+        return 'AcquisitionBundle:Inquiry:_panel-offer.html.twig';
+    }
+
+    /**
+     * Return the name of this panel.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->container->get('translator')->trans('crmp.acquisition.offer.plural');
+    }
+
+
+}
