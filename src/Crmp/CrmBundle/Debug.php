bfd2b351d65c168c455ac0eef91729bfe8c6f848
Mike Pretzlaw <pretzlaw@gmail.com>
2016-05-19 06:02:16 +0200

    Debugger helper to show the variable type or class name in case of objects.

---
+++ src/Crmp/CrmBundle/Debug.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Crmp\CrmBundle;
+
+
+class Debug
+{
+    public static function get_type($something)
+    {
+        if (is_object($something)) {
+            return get_class($something);
+        }
+
+        return gettype($something);
+    }
+}
