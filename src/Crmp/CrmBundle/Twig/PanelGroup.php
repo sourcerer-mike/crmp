f29b15b220e9b10443c9bd3f6fe9d2e89302e689
Mike Pretzlaw <pretzlaw@gmail.com>
2016-05-19 06:03:41 +0200

    PanelGroup: A panel can be removed by using the ID itself instead of an instance.

--- src/Crmp/CrmBundle/Twig/PanelGroup.php
+++ src/Crmp/CrmBundle/Twig/PanelGroup.php
@@ -11,18 +11,6 @@ class PanelGroup implements \IteratorAggregate
 
     protected $container;
 
-    /**
-     * @param ContainerInterface $container
-     *
-     * @return $this
-     */
-    public function setContainer($container)
-    {
-        $this->container = $container;
-
-        return $this;
-    }
-
     public function add(PanelInterface $panel)
     {
         $panel->setContainer($this->container);
@@ -44,9 +32,44 @@ class PanelGroup implements \IteratorAggregate
         return new \ArrayIterator($this->children);
     }
 
-    public function remove(PanelInterface $panel)
+    /**
+     * @param PanelInterface|string $panel
+     *
+     * @return $this
+     */
+    public function remove($panel)
     {
-        unset( $this->children[$panel->getId()] );
+        if ($panel instanceof PanelInterface) {
+            $panel = $panel->getId();
+        }
+
+        if ( ! is_scalar($panel)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Invalid argument of type "%s".'.
+                    ' Expected \\Crmp\\CrmBundle\\Twig\\PanelInterface or string containing the ID.',
+                    \Crmp\CrmBundle\Debug::get_type($panel)
+                )
+            );
+        }
+
+        if ( ! isset( $this->children[$panel] )) {
+            return $this;
+        }
+
+        unset( $this->children[$panel] );
+
+        return $this;
+    }
+
+    /**
+     * @param ContainerInterface $container
+     *
+     * @return $this
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
 
         return $this;
     }
