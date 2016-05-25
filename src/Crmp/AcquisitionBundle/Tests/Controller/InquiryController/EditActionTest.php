3db294c4e785e935aab79ad4ccea1df89c439fd2
Mike Pretzlaw <pretzlaw@gmail.com>
2016-05-25 02:56:29 +0200

    PHPUnit: Test that inquiry_* is accessible for users #30

---
+++ src/Crmp/AcquisitionBundle/Tests/Controller/InquiryController/EditActionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Crmp\AcquisitionBundle\Tests\Controller\InquiryController;
+
+
+use Crmp\AcquisitionBundle\Entity\Inquiry;
+use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
+
+class EditActionTest extends AuthTestCase
+{
+    public function testUserCanEditAnAddress()
+    {
+        /** @var Inquiry $someInquiry */
+        $someInquiry = $this->getRandomEntity('CrmpAcquisitionBundle:Inquiry');
+
+        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Inquiry', $someInquiry);
+
+        $routeParameters = ['id' => $someInquiry->getId()];
+        $client          = $this->createAuthorizedUserClient('GET', 'inquiry_edit', $routeParameters);
+        $response        = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertContains($someInquiry->getTitle(), $response->getContent());
+
+        $this->assertRoute($client, 'inquiry_edit', $routeParameters);
+    }
+}
---
+++ src/Crmp/AcquisitionBundle/Tests/Controller/InquiryController/ShowActionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Crmp\AcquisitionBundle\Tests\Controller\InquiryController;
+
+
+use Crmp\AcquisitionBundle\Entity\Inquiry;
+use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
+
+class ShowActionTest extends AuthTestCase
+{
+    public function testUserCanAccessTheList()
+    {
+        /** @var Inquiry $someInquiry */
+        $someInquiry = $this->getRandomEntity('CrmpAcquisitionBundle:Inquiry');
+
+        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Inquiry', $someInquiry);
+
+        $client   = $this->createAuthorizedUserClient('GET', 'inquiry_show', ['id' => $someInquiry->getId()]);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertContains($someInquiry->getTitle(), $response->getContent());
+    }
+}
