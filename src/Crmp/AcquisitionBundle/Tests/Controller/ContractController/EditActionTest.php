38fdb865578bc78c64c6be51bd2f3e49ca660370
Mike Pretzlaw <pretzlaw@gmail.com>
2016-05-25 02:54:26 +0200

    PHPUnit: Test that contract_* is accessible for users #30
    Added new fixtures for offers and contracts

---
+++ src/Crmp/AcquisitionBundle/Tests/Controller/ContractController/EditActionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Crmp\AcquisitionBundle\Tests\Controller\ContractController;
+
+
+use Crmp\AcquisitionBundle\Entity\Contract;
+use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
+
+class EditActionTest extends AuthTestCase
+{
+    public function testUserCanEditAnAddress()
+    {
+        /** @var Contract $someContract */
+        $someContract = $this->getRandomEntity('CrmpAcquisitionBundle:Contract');
+
+        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Contract', $someContract);
+
+        $routeParameters = ['id' => $someContract->getId()];
+        $client          = $this->createAuthorizedUserClient('GET', 'contract_edit', $routeParameters);
+        $response        = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertContains($someContract->getTitle(), $response->getContent());
+
+        $this->assertRoute($client, 'contract_edit', $routeParameters);
+    }
+}
---
+++ src/Crmp/AcquisitionBundle/Tests/Controller/ContractController/ShowActionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Crmp\AcquisitionBundle\Tests\Controller\ContractController;
+
+
+use Crmp\AcquisitionBundle\Entity\Contract;
+use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
+
+class ShowActionTest extends AuthTestCase
+{
+    public function testUserCanAccessTheList()
+    {
+        /** @var Contract $someContract */
+        $someContract = $this->getRandomEntity('CrmpAcquisitionBundle:Contract');
+
+        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Contract', $someContract);
+
+        $client   = $this->createAuthorizedUserClient('GET', 'contract_show', ['id' => $someContract->getId()]);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertContains($someContract->getTitle(), $response->getContent());
+    }
+}
