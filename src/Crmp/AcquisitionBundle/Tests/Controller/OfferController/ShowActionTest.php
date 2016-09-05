443845a36f471bf76ebfe49f1ae56d5a5a0ab3fc
Mike Pretzlaw <pretzlaw@gmail.com>
2016-09-05 17:37:19 +0200

    Real UnitTest for the OfferController::showAction (github #31)
    UnitTests have been made using the WebTest.
    This is wrong as WebTests already stand for integration tests.
    So they were replaced by testing the interface and internal calls.

--- src/Crmp/AcquisitionBundle/Tests/Controller/OfferController/ShowActionTest.php
+++ src/Crmp/AcquisitionBundle/Tests/Controller/OfferController/ShowActionTest.php
@@ -3,55 +3,65 @@
 namespace Crmp\AcquisitionBundle\Tests\Controller\OfferController;
 
 
+use Crmp\AcquisitionBundle\Controller\OfferController;
 use Crmp\AcquisitionBundle\Entity\Offer;
 use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
-use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Form\Form;
 
-class ShowActionTest extends AuthTestCase
+/**
+ * Show offer
+ *
+ * It is possible to view a single offer.
+ *
+ * @package Crmp\AcquisitionBundle\Tests\Controller\OfferController
+ */
+class ShowActionTest extends KernelTestCase
 {
-    public function testUserCanAccessTheList()
+    /**
+     * Assert that the render method will be called.
+     *
+     * - Should use the 'CrmpAcquisitionBundle:Offer:show.html.twig'
+     * - Should have the offer
+     * - Should have a delete form
+     */
+    public function testItRendersAnOffer()
     {
-        /** @var Offer $someOffer */
-        $someOffer = $this->getRandomEntity('CrmpAcquisitionBundle:Offer');
+        // Preconditions
+        $stub = new Offer();
+        $stub
+            ->setTitle(uniqid())
+            ->setContent(uniqid())
+            ->setPrice(1234)
+            ->setStatus(0);
 
-        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Offer', $someOffer);
+        $controller = $this->getMockBuilder(OfferController::class)->setMethods(['render', 'createDeleteForm'])
+                           ->getMock();
 
-        $client   = $this->createAuthorizedUserClient('GET', 'crmp_acquisition_offer_show', ['id' => $someOffer->getId()]);
-        $response = $client->getResponse();
 
-        $this->assertTrue($response->isSuccessful());
-        $this->assertContains($someOffer->getTitle(), $response->getContent());
+        $deleteForm = $this->getMockBuilder(Form::class)->disableOriginalConstructor()->getMock();
+        $deleteForm->method('createView')->willReturn(uniqid());
+        $controller->method('createDeleteForm')->willReturn($deleteForm);
+
+        // Assertions
+        $controller->expects($this->once())->method('render')->will(
+            $this->returnCallback(
+                function ($template, $data) use ($stub, $deleteForm) {
+                    \PHPUnit_Framework_Assert::assertEquals('CrmpAcquisitionBundle:Offer:show.html.twig', $template);
+                    \PHPUnit_Framework_Assert::assertEquals($stub, $data['offer']);
+                    \PHPUnit_Framework_Assert::assertEquals($deleteForm->createView(), $data['delete_form']);
+                }
+            )
+        );
+
+        /** @var OfferController $controller */
+        $controller->setContainer(static::$kernel->getContainer());
+        $controller->showAction($stub);
     }
 
-    public function testRelatedCustomerIsShownWithOffer()
+    protected function setUp()
     {
-        $em = static::createClient()->getContainer()->get('doctrine.orm.entity_manager');
-        $repo = $em->getRepository('CrmpAcquisitionBundle:Offer');
-
-        $all = $repo->createQueryBuilder('offer')
-            ->where('offer.customer IS NOT NULL')
-            ->setMaxResults(5)
-            ->getQuery()
-            ->getArrayResult();
-
-        $this->assertNotEmpty($all);
-        $offerData = $all[array_rand($all)];
-
-        $this->assertArrayHasKey('id', $offerData);
-        $offerId = $offerData['id'];
-
-        // load offer
-        $this->assertNotEmpty($offerId);
-        $offer = $repo->find($offerId);
-        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Offer', $offer);
-
-        // get customer
-        $customer = $offer->getCustomer();
-        $this->assertNotNull($customer);
-
-        // request
-        $client = $this->createAuthorizedUserClient('GET', 'crmp_acquisition_offer_show', ['id' => $offerId]);
-
-        $this->assertContains($customer->getName(), $client->getResponse()->getContent());
+        parent::setUp();
+        self::bootKernel();
     }
 }
