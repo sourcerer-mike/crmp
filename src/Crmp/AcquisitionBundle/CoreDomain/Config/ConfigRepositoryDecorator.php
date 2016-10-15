2e599e20a1770c64936dd44ee1f6932e3f510b7a
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-16 00:35:35 +0200

    Stub some configuration for development

---
+++ src/Crmp/AcquisitionBundle/CoreDomain/Config/ConfigRepositoryDecorator.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Crmp\AcquisitionBundle\CoreDomain\Config;
+
+
+use Crmp\CrmBundle\CoreDomain\Config\ConfigRepositoryInterface;
+
+class ConfigRepositoryDecorator implements ConfigRepositoryInterface
+{
+    /**
+     * Decorated object.
+     *
+     * @var ConfigRepositoryInterface
+     */
+    protected $configRepository;
+
+    public function __construct(ConfigRepositoryInterface $configRepository)
+    {
+        $this->configRepository = $configRepository;
+    }
+
+    /**
+     * Fetch a configuration from the storage.
+     *
+     * @param string $path Node of the configuration.
+     *
+     * @deprecated 1.0.0 Only for development purposes as long as there is no storage in the DB.
+     *
+     * @return string|null The value or null if nothing found.
+     */
+    public function get($path)
+    {
+        // Deprecated: Here we stub one configuration.
+        if ('crmp_acquisition.offer.states' == $path) {
+            return [
+                'crmp_acquisition.offer.states.open'     => 0,
+                'crmp_acquisition.offer.states.closed'   => 1,
+                'crmp_acquisition.offer.states.rejected' => 2,
+            ];
+        }
+
+        // pass to repo
+        return $this->configRepository->get($path);
+    }
+}
