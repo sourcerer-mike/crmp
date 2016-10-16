2b0def309212988f1ef06bffdcbb361fbb98ed1c
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-16 18:35:31 +0200

    OfferController::editAction(): Tests for edit action with core domain repository

--- src/Crmp/AcquisitionBundle/Tests/Controller/OfferController/EditActionTest.php
+++ src/Crmp/AcquisitionBundle/Tests/Controller/OfferController/EditActionTest.php
@@ -3,25 +3,93 @@
 namespace Crmp\AcquisitionBundle\Tests\Controller\OfferController;
 
 
+use Crmp\AcquisitionBundle\Controller\OfferController;
+use Crmp\AcquisitionBundle\CoreDomain\Offer\OfferRepository;
 use Crmp\AcquisitionBundle\Entity\Offer;
-use Crmp\CrmBundle\Tests\Controller\AuthTestCase;
+use Crmp\CrmBundle\Tests\Controller\AbstractControllerTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
-class EditActionTest extends AuthTestCase
+/**
+ * Asserting correct edit actions on an offer.
+ *
+ * @see OfferController::editAction()
+ *
+ * @package Crmp\AcquisitionBundle\Tests\Controller\OfferController
+ */
+class EditActionTest extends AbstractControllerTestCase
 {
-    public function testUserCanEditAnAddress()
+    public function testCreatesEditFormForOffer()
     {
-        /** @var Offer $someOffer */
-        $someOffer = $this->getRandomEntity('CrmpAcquisitionBundle:Offer');
+        $offer = new Offer();
+        $offer->setTitle('the offer');
+        $offer->setStatus(-1);
+        $offer->setPrice(44);
 
-        $this->assertInstanceOf('\\Crmp\\AcquisitionBundle\\Entity\\Offer', $someOffer);
+        $controller = $this->controllerMock = $this->controllerBuilder->getMock();
 
-        $routeParameters = ['id' => $someOffer->getId()];
-        $client          = $this->createAuthorizedUserClient('GET', 'crmp_acquisition_offer_edit', $routeParameters);
-        $response        = $client->getResponse();
+        $this->createCreateFormMock()
+             ->expects($this->atLeastOnce())
+             ->method('isSubmitted')
+             ->willReturn(false);
 
-        $this->assertTrue($response->isSuccessful());
-        $this->assertContains($someOffer->getTitle(), $response->getContent());
+        $this->createDeleteFormMock();
 
-        $this->assertRoute($client, 'crmp_acquisition_offer_edit', $routeParameters);
+        $this->expectRendering('offer', $offer, 'CrmpAcquisitionBundle:Offer:edit.html.twig');
+
+        /** @var OfferController $controller */
+        $controller->editAction($this->createMock(Request::class), $offer);
+    }
+
+    public function testItDelegatesSaveOperations()
+    {
+        $offer = new Offer();
+        $offer->setPrice(44);
+        $offer->setStatus(-14);
+        $offer->setTitle('the offer');
+
+        $controller = $this->controllerMock = $this->controllerBuilder->getMock();
+
+        $controller->expects($this->atLeastOnce())
+                   ->method('get')
+                   ->with('crmp.offer.repository')
+                   ->willReturn(
+                       $repoMock = $this->createMock(OfferRepository::class)
+                   );
+
+        $repoMock->expects($this->once())
+                 ->method('update')
+                 ->with($offer);
+
+
+        $createFormMock = $this->createCreateFormMock();
+
+        $createFormMock->expects($this->atLeastOnce())
+                       ->method('isSubmitted')
+                       ->willReturn(true);
+
+        $createFormMock->expects($this->atLeastOnce())
+                       ->method('isValid')
+                       ->willReturn(true);
+
+        /** @var OfferController $controller */
+        $controller->editAction($this->createMock(Request::class), $offer);
+    }
+
+    protected function setUp()
+    {
+        $this->controllerClass = OfferController::class;
+
+        parent::setUp();
+
+        $this->controllerBuilder->setMethods(
+            [
+                'createDeleteForm',
+                'createForm',
+                'render',
+                'get',
+                'getUser',
+                'redirectToRoute',
+            ]
+        );
     }
 }
