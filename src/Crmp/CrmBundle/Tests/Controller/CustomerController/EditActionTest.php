30b3bca3dfc767a09615e5928f23d0bcae006603
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-16 11:09:02 +0200

    CustomerController: Check if edit will be delegated
    Edits should be delegated to the repository.

--- src/Crmp/CrmBundle/Tests/Controller/CustomerController/EditActionTest.php
+++ src/Crmp/CrmBundle/Tests/Controller/CustomerController/EditActionTest.php
@@ -4,6 +4,7 @@ namespace Crmp\CrmBundle\Tests\Controller\CustomerController;
 
 
 use Crmp\CrmBundle\Controller\CustomerController;
+use Crmp\CrmBundle\CoreDomain\Customer\CustomerRepository;
 use Crmp\CrmBundle\Entity\Customer;
 use Crmp\CrmBundle\Tests\Controller\AbstractControllerTestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,6 +29,8 @@ class EditActionTest extends AbstractControllerTestCase
                 'createDeleteForm',
                 'createForm',
                 'render',
+                'get',
+                'redirectToRoute',
             ]
         );
     }
@@ -47,6 +50,38 @@ class EditActionTest extends AbstractControllerTestCase
         $this->createDeleteFormMock();
 
         $this->expectRendering('customer', $customer, 'CrmpCrmBundle:Customer:edit.html.twig');
+
+        /** @var CustomerController $controller */
+        $controller->editAction($this->createMock(Request::class), $customer);
+    }
+
+    public function testItDelegatesSaveOperations()
+    {
+        $customer = new Customer();
+        $customer->setName('the name');
+
+        $controller = $this->controllerMock = $this->controllerBuilder->getMock();
+
+        $controller->expects($this->atLeastOnce())
+                   ->method('get')
+                   ->with('crmp.customer.repository')
+                   ->willReturn(
+                       $repoMock = $this->createMock(CustomerRepository::class)
+                   );
+
+        $repoMock->expects($this->once())
+            ->method('update')
+            ->with($customer);
+
+        $createFormMock = $this->createCreateFormMock();
+
+        $createFormMock->expects($this->atLeastOnce())
+                       ->method('isSubmitted')
+                       ->willReturn(true);
+
+        $createFormMock->expects($this->atLeastOnce())
+                       ->method('isValid')
+                       ->willReturn(true);
 
         /** @var CustomerController $controller */
         $controller->editAction($this->createMock(Request::class), $customer);
