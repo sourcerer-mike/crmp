3c19dde8d0ca33840caa37035c070332160ffb0f
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-29 07:26:24 +0200

    UnitTests: More intuitive assertion on render method.
    Expectations for the rendering were made using the ::expectRendering Method.
    It had a different signature than the actual ::render method,
    so it was not very intuitive.
    The new ::expectRenderingWith has the correct signature.

--- src/Crmp/CrmBundle/Tests/Controller/AbstractControllerTestCase.php
+++ src/Crmp/CrmBundle/Tests/Controller/AbstractControllerTestCase.php
@@ -5,6 +5,7 @@ namespace Crmp\CrmBundle\Tests\Controller;
 
 
 use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractControllerTestCase extends \PHPUnit_Framework_TestCase
 {
@@ -35,19 +36,6 @@ abstract class AbstractControllerTestCase extends \PHPUnit_Framework_TestCase
      */
     protected $controllerMock;
 
-    protected function expectRendering($entityKey, $entity, $template)
-    {
-        $self               = $this;
-        $customerController = $this->controllerMock;
-
-        $customerController->expects($this->once())->method('render')->willReturnCallback(
-            function ($view, $parameters = array(), $response = null) use ($self, $entity, $template, $entityKey) {
-                $self->assertEquals($template, $view);
-                $self->assertEquals($entity, $parameters[$entityKey]);
-            }
-        );
-    }
-
     protected function createCreateFormMock()
     {
         $this->controllerMock->expects($this->any())->method('createForm')->willReturn(
@@ -64,6 +52,63 @@ abstract class AbstractControllerTestCase extends \PHPUnit_Framework_TestCase
         );
 
         return $deleteFormMock;
+    }
+
+    /**
+     * Check if the rendering will be called correctly.
+     *
+     * @deprecated 1.0.0 Use the more intuitive ::expectRenderingWith() method.
+     *
+     * @param $entityKey
+     * @param $entity
+     * @param $template
+     */
+    protected function expectRendering($entityKey, $entity, $template)
+    {
+        $self               = $this;
+        $customerController = $this->controllerMock;
+
+        $customerController->expects($this->once())->method('render')->willReturnCallback(
+            function ($view, $parameters = array(), $response = null) use ($self, $entity, $template, $entityKey) {
+                $self->assertEquals($template, $view);
+                $self->assertEquals($entity, $parameters[$entityKey]);
+            }
+        );
+    }
+
+    /**
+     * Check if the rendering will be called correctly.
+     *
+     * @see \Symfony\Bundle\FrameworkBundle\Controller\Controller::render()
+     *
+     * @param string   $expectedView       String what the view should be.
+     * @param array    $expectedParameters Subset or complete array which data is required at last.
+     * @param Response $expectedResponse   The complete expected response.
+     */
+    protected function expectRenderingWith($expectedView, $expectedParameters = array(), $expectedResponse = null)
+    {
+        $self = $this;
+
+        $this->controllerMock->expects($this->once())->method('render')->willReturnCallback(
+            function ($view, $parameters = array(), $response = null) use (
+                $self,
+                $expectedView,
+                $expectedParameters,
+                $expectedResponse
+            ) {
+                if ($expectedView) {
+                    $self->assertEquals($expectedView, $view);
+                }
+
+                if ($expectedParameters) {
+                    $self->assertArraySubset($expectedParameters, $parameters);
+                }
+
+                if ($response) {
+                    $self->assertEquals($expectedResponse, $response);
+                }
+            }
+        );
     }
 
     protected function setUp()
