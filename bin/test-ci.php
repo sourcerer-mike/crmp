2c6eb30335f3b045f87832a6d4d8a642415063fe
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-30 17:06:22 +0100

    QA: Forward exit code

--- bin/test-ci.php
+++ bin/test-ci.php
@@ -5,10 +5,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $yml = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__.'/../.travis.yml'));
 
+$exitCode = 0;
 foreach ($yml['script'] as $testScript) {
     if (false !== strpos($testScript, 'phpunit')) {
         $testScript = str_replace('--coverage-clover var/phpunit/coverage.xml', '', $testScript);
     }
 
-    system($testScript);
+    $returnVar = 0;
+
+    system($testScript, $returnVar);
+    $exitCode += $returnVar;
 }
+
+exit($exitCode);
