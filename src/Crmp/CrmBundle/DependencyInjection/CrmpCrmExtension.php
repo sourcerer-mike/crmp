f2c86a4c2b1b3e48e9941934dd9eef4fa6614f36
Mike Pretzlaw <pretzlaw@gmail.com>
2016-10-30 20:16:32 +0100

    Swap all panels to its own yml file

--- src/Crmp/CrmBundle/DependencyInjection/CrmpCrmExtension.php
+++ src/Crmp/CrmBundle/DependencyInjection/CrmpCrmExtension.php
@@ -25,6 +25,7 @@ class CrmpCrmExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('domain.yml');
+        $loader->load('panels.yml');
         $loader->load('services.yml');
     }
 }
