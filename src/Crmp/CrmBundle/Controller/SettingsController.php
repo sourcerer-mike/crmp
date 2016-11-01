0e3cc7194d7fa86f4c5e8cc39ff6e45183397844
Mike Pretzlaw <pretzlaw@gmail.com>
2016-11-01 03:15:25 +0100

    SettingsController: Handle incoming data for all forms.
    Form or settings can be registered using panels.
    Those forms were not used for data validation yet.
    The controller applies incoming data on all forms
    so that they can response to invalid data.

--- src/Crmp/CrmBundle/Controller/SettingsController.php
+++ src/Crmp/CrmBundle/Controller/SettingsController.php
@@ -3,6 +3,7 @@
 namespace Crmp\CrmBundle\Controller;
 
 use AppBundle\Controller\AbstractCrmpController;
+use Crmp\CrmBundle\Twig\AbstractSettingsPanel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,8 +28,44 @@ class SettingsController extends AbstractCrmpController
      */
     public function indexAction(Request $request)
     {
-        return $this->render(
-            'CrmpCrmBundle:Settings:index.html.twig'
-        );
+        // Flag the current state
+        $isValid     = true;
+        $isSubmitted = false;
+
+        // Data that shall be written
+        $data = [];
+
+        // forward request to form
+        foreach ($this->get('crmp_crm.settings.panels') as $panel) {
+            /** @var AbstractSettingsPanel $panel */
+
+            $form        = $panel->getForm();
+            $isSubmitted = $isSubmitted || $form->isSubmitted();
+
+            if ($form->isSubmitted()) {
+                // something already happened => skip this form
+                continue;
+            }
+
+            $form->handleRequest($request);
+
+            $isValid = $isValid && $form->isValid();
+
+            if (! $form->isSubmitted() || ! $form->isValid()) {
+                // nothing submitted here or invalid => do not persist this data
+                continue;
+            }
+
+            // persist data
+            $data = array_merge($data, $form->getData());
+        }
+
+        if ($isSubmitted && $isValid) {
+            // form has been send => redirect to prevent additional save on reload
+            return $this->redirectToRoute('crmp_crm_settings');
+        }
+
+
+        return $this->render('CrmpCrmBundle:Settings:index.html.twig');
     }
 }
--- src/Crmp/CrmBundle/Tests/Controller/SettingsController/IndexActionTest.php
+++ src/Crmp/CrmBundle/Tests/Controller/SettingsController/IndexActionTest.php
@@ -5,7 +5,12 @@ namespace Crmp\CrmBundle\Tests\Controller\SettingsController;
 
 
 use Crmp\CrmBundle\Controller\SettingsController;
+use Crmp\CrmBundle\Panels\Settings\General;
 use Crmp\CrmBundle\Tests\Controller\AbstractControllerTestCase;
+use Crmp\CrmBundle\Twig\PanelGroup;
+use Crmp\CrmBundle\Twig\PanelInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -21,10 +26,106 @@ class IndexActionTest extends AbstractControllerTestCase
      */
     protected $controllerMock;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $formMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $generalPanelMock;
+
+    /**
+     * @var PanelGroup
+     */
+    private $panelsStub;
+
+    /**
+     * Redirect on submit.
+     *
+     * The user made a valid submit.
+     * When the user presses refresh he would be asked to submit again by the browser.
+     * To suppress that a redirect is made.
+     *
+     */
+    public function testItRedirectsOnValidSubmits()
+    {
+        $panelMock = $this->createPanelMock();
+
+        $this->createFormMock($panelMock, ['isSubmitted' => true, 'isValid' => true]);
+
+        $this->expectRedirectToRoute('crmp_crm_settings');
+
+        $this->controllerMock->indexAction(new Request());
+    }
+
     public function testItShowsListOfSettings()
     {
         $this->expectRenderingWith('CrmpCrmBundle:Settings:index.html.twig');
 
         $this->controllerMock->indexAction(new Request());
+    }
+
+    public function testWrongSubmitsLeadBackToList()
+    {
+        $panelMock = $this->createPanelMock();
+
+        $this->createFormMock($panelMock, ['isSubmitted' => false, 'isValid' => false]);
+        $this->createFormMock($panelMock, ['isSubmitted' => false, 'isValid' => true]);
+        $this->createFormMock($panelMock, ['isSubmitted' => true, 'isValid' => true]);
+
+        $this->expectRenderingWith('CrmpCrmBundle:Settings:index.html.twig');
+
+        $this->controllerMock->indexAction(new Request());
+    }
+
+    /**
+     * Set the form in a panel mock.
+     *
+     * @param $panelMock
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function createFormMock($panelMock, $methodToReturnValue)
+    {
+        $formMock = $this->getMockBuilder(Form::class)
+                         ->disableOriginalConstructor()
+                         ->setMethods(['getData', 'handleRequest', 'isSubmitted', 'isValid'])
+                         ->getMock();
+
+        $panelMock->expects($this->once())
+                  ->method('getForm')
+                  ->willReturn($formMock);
+
+        foreach ($methodToReturnValue as $method => $returnValue) {
+            $formMock->expects($this->any())
+                     ->method($method)
+                     ->willReturn($returnValue);
+        }
+
+        return $formMock;
+    }
+
+    protected function createPanelMock($class = General::class)
+    {
+        $panelMock = $this->getMockBuilder($class)
+                          ->setMethods(['getForm'])
+                          ->getMock();
+
+        /** @var PanelInterface $panelMock */
+        $this->panelsStub->add($panelMock);
+
+        return $panelMock;
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->panelsStub = new PanelGroup();
+        $this->panelsStub->setContainer(new Container());
+
+        $this->mockService('crmp_crm.settings.panels', $this->panelsStub);
     }
 }
