92d053d27ea28c8d6d975ed36df3ee6e70a875f6
Mike Pretzlaw <pretzlaw@gmail.com>
2016-11-07 13:51:45 +0100

    Deploy: Allow override the environment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+crmp_env=${crmp_env-prod}
+
 composer install
 
-bin/console cache:clear -e prod
-bin/console assets:install web --symlink --relative
-bin/console doctrine:schema:update -f
+bin/console cache:clear -e ${crmp_env}
+bin/console assets:install web -e ${crmp_env} --symlink --relative
+bin/console doctrine:schema:update -e ${crmp_env} -f
 
 bin/populate.sh
 
-bin/console assetic:dump -e prod web
+bin/console assetic:dump -e ${crmp_env} web
