8128951fe5fa54cd12660ca6fa1941fdb8df3483
Mike Pretzlaw <pretzlaw@gmail.com>
2016-11-09 12:02:02 +0100

    Split panels from other services (github #42)

--- src/Crmp/AcquisitionBundle/DependencyInjection/CrmpAcquisitionExtension.php
+++ src/Crmp/AcquisitionBundle/DependencyInjection/CrmpAcquisitionExtension.php
@@ -23,6 +23,7 @@ class CrmpAcquisitionExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('panels.yml');
         $loader->load('services.yml');
         $loader->load('domain.yml');
     }
